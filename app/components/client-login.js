547c0c7a2fbee191f6260e4f290ca1dfe189b4bf
Minneh Mugo <minneh.mugo@gmail.com>
2017-04-10 13:01:30 +0300

    add save new client functionality and include bootstrap styling

--- app/components/client-login.js
+++ app/components/client-login.js
@@ -1,4 +1,22 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  addNewClient: true,
+  actions: {
+   clientFormShow() {
+     this.set('addNewClient', false);
+   },
+  //  component behaviour for save button
+   saveClient1(){
+    // collects all of the data that was entered in the input fields and sets them as values in a hash of keys with the same names.
+     var params = {
+       firstName: this.get('firstName'),
+       lastName: this.get('lastName'),
+       username: this.get('username'),
+       password: this.get('password')
+     };
+     this.set('addNewQuestion', true); //hide form again after each field's value is collected
+     this.sendAction('saveClient2', params); //emits the action saveQuestion2 sending with it the params hash that we just made
+   }
+ }
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model(){
+      // this.store: the Firebase data store we've set up for our application
+      // the findAll() method with the argument client instructs Ember Data to find all records of the type client in the store, and return them to our application.
+       clients: this.store.findAll('client');
+  },
+  actions:{
+    // save question function to save questions to db
+    saveClient3(params){
+      var newClient = this.store.createRecord('client', params); //use params to create new question record in the store
+      newClient.save(); //save new question
+
+      // move to index page and see our new question in the list
+      this.transitionTo('client');
+    }
+  }
 });
