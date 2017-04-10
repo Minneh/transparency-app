8dceefbbfc4a7a6a46c35fa042d8b7ff90ebf439
Minneh Mugo <minneh.mugo@gmail.com>
2017-04-10 18:05:47 +0300

    add new-service and modify client to accomodate it

--- app/components/new-service.js
+++ app/components/new-service.js
@@ -1,4 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+
+  actions: {
+  //  component behaviour for save button
+   saveService(){
+    // collects all of the data that was entered in the input fields and sets them as values in a hash of keys with the same names.
+     var params = {
+       name: this.get('name'),
+       price: this.get('price'),
+     };
+
+     this.sendAction('saveService', params); //emits the action saveQuestion2 sending with it the params hash that we just made
+   }
+ }
 });
--- app/routes/client.js
+++ app/routes/client.js
@@ -3,5 +3,15 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params){
     return this.store.findRecord('client', params.client_id);
+  },
+  actions:{
+    // save question function to save questions to db
+    saveService(params){
+      var newService = this.store.createRecord('service', params); //use params to create new question record in the store
+      newService.save(); //save new question
+
+      // move to index page and see our new question in the list
+      this.transitionTo('client');
+    }
   }
 });
