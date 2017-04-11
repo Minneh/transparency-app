f373dabd9862a7723f8a5e6413aed7fa11ed3cf3
Minneh Mugo <minneh.mugo@gmail.com>
2017-04-11 15:17:35 +0300

    - major modifications: * add approveService action * render client-detail in admin page * add Cliff to footer * add if statement for dynamic display of approve button

--- app/components/client-detail.js
+++ app/components/client-detail.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  isButtonShowing: false,
+  actions: {
+    approveService: function() {
+      this.set('isButtonShowing', true);
+    }
+  }
 });
